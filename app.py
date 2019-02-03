99afe88459ba96826b225ad2d30611817c905024
Tomer Aharoni <tomeraharoni@Tomers-MacBook-Pro.local>
2019-02-02 19:37:20 -0500

    added buttons to bot

--- app.py
+++ app.py
@@ -33,7 +33,15 @@ def receive_message():
                 # This if segment is called whenever a user click a button inside the facebook chat
                 if 'postback' in message and 'payload' in message['postback']:
                     payload = message['postback']['payload'].strip()
-                    print("payload: ", payload)
+                    recipient_id = message['sender']['id']
+                    if payload == 'dmv':
+                        select_help_type_message(recipient_id, 'dmv')
+                    elif payload == 'immigration':
+                        select_help_type_message(
+                            recipient_id, 'immigration')
+                    elif payload == 'medical':
+                        select_help_type_message(recipient_id, 'medical')
+
                 # This segment is called when a user sends a text message
                 if message.get('message'):
                     # Facebook Messenger ID for user so we know where to send response back to
@@ -45,7 +53,8 @@ def receive_message():
                             serve_bot(txt, lang)
                         else:
                             lang = get_lang_from_text(txt)
-                            select_help_type_message(recipient_id)
+                            select_help_type_message(
+                                recipient_id, 'default')
                             # serve_bot(txt, lang)
                         print("*" * 20)
                         print("LANG: " + lang)
@@ -96,35 +105,106 @@ def get_lang_from_text(txt):
     # uses PyMessenger to send response to user
 
 
-def select_help_type_message(recipient_id, response=None):
+def select_help_type_message(recipient_id, pl, response=None):
     # sends user the text message provided via input response parameter
          # construct payload and send it
-    payload = {
-        "recipient": {"id": recipient_id},
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "button",
-                    "text": "Hello! I'm lil' P. I'm here to help you! What help do you need today?",
-                    "buttons":  [
-                        {
-                            "type": "postback",
-                            "title": "Immigration",
-                            "payload": "immigration"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "DMV",
-                            "payload": "dmv"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Medical",
-                            "payload": "medical"
-                        },
-                    ]
-                }}}}
+    if pl == 'immigration':
+        payload = {
+            "recipient": {"id": recipient_id},
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "Sure! These are some immigration documents that we're currently supporting",
+                        "buttons":  [
+                            {
+                                "type": "postback",
+                                "title": "I-9",
+                                "payload": "I-9"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "I-765",
+                                "payload": "I-765"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "I-20",
+                                "payload": "I-20"
+                            },
+                        ]
+                    }}}}
+
+    elif pl == 'medical':
+        payload = {
+            "recipient": {"id": recipient_id},
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "Sure! These are some medical documents that we're currently supporting",
+                        "buttons":  [
+                            {
+                                "type": "postback",
+                                "title": "Application for insurance",
+                                "payload": "insurance"
+                            },
+                        ]
+                    }}}}
+
+    elif pl == 'dmv':
+        payload = {
+            "recipient": {"id": recipient_id},
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "Sure! These are some DMV related documents that we're currently supporting",
+                        "buttons":  [
+                            {
+                                "type": "postback",
+                                "title": "MV-44",
+                                "payload": "MV-44"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "MV-45",
+                                "payload": "MV-45"
+                            },
+                        ]
+                    }}}}
+
+    else:
+        payload = {
+            "recipient": {"id": recipient_id},
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": "Hello! I'm lil' P. I'm here to help you! What help do you need today?",
+                        "buttons":  [
+                            {
+                                "type": "postback",
+                                "title": "Immigration",
+                                "payload": "immigration"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "DMV",
+                                "payload": "dmv"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "Medical",
+                                "payload": "medical"
+                            },
+                        ]
+                    }}}}
+
     r = requests.post(
         'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(ACCESS_TOKEN), json=payload)
     return "success"
